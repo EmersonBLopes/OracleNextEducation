57e0de5e5445f042e11269fedef5eb9cf54ecbf1
EmersonBLopes <emersonbasiliolopes@gmail.com>
2022-10-21 22:54:04 -0300

    Conclusão da segunda aula do curso de Javascript a linguagem da web

--- front_end_t3/javascript_a_linguagem_da_web/1-meuPrimeiroPrograma.js
+++ front_end_t3/javascript_a_linguagem_da_web/1-meuPrimeiroPrograma.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+console.log("Meu Primeiro programa: trabalhando com variáveis");
+
+const idade = 26;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
