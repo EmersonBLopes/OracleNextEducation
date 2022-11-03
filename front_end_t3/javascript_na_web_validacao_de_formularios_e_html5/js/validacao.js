10341e96cfe543c688ff8aabbc6e210749de80a4
EmersonBLopes <emersonbasiliolopes@gmail.com>
2022-11-02 21:42:23 -0300

    Conclusão da aula 03, validação de formularios com JS

--- front_end_t3/javascript_na_web_validacao_de_formularios_e_html5/js/validacao.js
+++ front_end_t3/javascript_na_web_validacao_de_formularios_e_html5/js/validacao.js
@@ -1,25 +1,73 @@
-const dataNascimento = document.querySelector("#nascimento");
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo
 
-dataNascimento.addEventListener("blur", evento =>{
-  validaDataNascimento(evento.target);
-})
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
 
-function validaDataNascimento(input){
-
-  const dataRecebida = new Date(input.value);
-  let mensagem = "";
-
-  if(!maiorQue18(dataRecebida)){
-    mensagem = "Você deve ser maior que 18 anos para se cadastrar";
-  }
-
-  input.setCustomValidity(mensagem);
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
+    }
 }
 
-function maiorQue18(data){
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
 
-  const dataAtual = new Date()
-  const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo de nome não pode estar vazio.'
+    },
+    email: {
+        valueMissing: 'O campo de email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é válido.'
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.'
+    },
+    dataNascimento: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    }
+}
 
-  return dataMais18 <= dataAtual;
+const validadores = {
+    dataNascimento:input => validaDataNascimento(input)
+}
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = ''
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+    
+    return mensagem
+}
+
+function validaDataNascimento(input) {
+    const dataRecebida = new Date(input.value)
+    let mensagem = ''
+
+    if(!maiorQue18(dataRecebida)) {
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function maiorQue18(data) {
+    const dataAtual = new Date()
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+
+    return dataMais18 <= dataAtual
 }
