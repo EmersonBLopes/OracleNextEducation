c4c90cb8b457eb7f044600aef4a4cf3cd69b46a7
EmersonBLopes <emersonbasiliolopes@gmail.com>
2022-11-12 21:19:02 -0300

    verificações de instancia e existencia da função autenticar dentro de um objeto

--- front_end_t3/javascript_programando_a_orientacao_a_objetos/SistemaAutenticacao.js
+++ front_end_t3/javascript_programando_a_orientacao_a_objetos/SistemaAutenticacao.js
@@ -1,5 +1,15 @@
 export class SistemaAutenticacao{
+
   static login(autenticavel,senha){
-    return autenticavel.autenticar(senha);
+    if(SistemaAutenticacao.ehAutenticavel(autenticavel)){
+      return autenticavel.autenticar(senha);
+   }
+    
+    return false;
   }
+
+  static ehAutenticavel(autenticavel){
+    return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function;
+  }
+
 }
